eed2a547af7e359210ebd248dd9156b34c2507fa
User <User@YD01-08>
2019-12-19 13:37:37 +0900

    d

--- Hellowolrd/src/conditions/forMultiplicationTableExample.java
+++ Hellowolrd/src/conditions/forMultiplicationTableExample.java
@@ -11,14 +11,18 @@ public class forMultiplicationTableExample {
 //			}
 //
 //		}
-
+		
 		for (int m = 2; m <= 9; m++) {
-			System.out.print("***  " + m + "단 *** ");
-		}
-		for (int m = 2; m <= 9; m++) {
-			for (int n = 1; n <= 9; n++) {
-				System.out.print(m + " X " + n + "=" + (m * n) + " ");
-			}
+			System.out.print("* " + m + " 단 *    ");
+		} System.out.println();
+		for (int n = 1; n <= 9; n++) {
+			for (int m = 2; m <= 9; m++) {
+				if ((m*n) % 10 <= 0.99) {
+					System.out.print(m + " X " + n + "=" + (m * n) + "  ");
+					}else
+				System.out.print(m + " X " + n + "=" + (m * n) + "   ");
+				
+			}System.out.println();
 		}
 	}
 
