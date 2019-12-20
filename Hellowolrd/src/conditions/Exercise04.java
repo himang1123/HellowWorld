9d1018020121dda1a2d5b1b6640e77da89f3f882
User <User@YD01-08>
2019-12-20 09:54:42 +0900

    d

--- Hellowolrd/src/conditions/Exercise04.java
+++ Hellowolrd/src/conditions/Exercise04.java
@@ -1,9 +1,17 @@
 package conditions;
 
 public class Exercise04 {
-public static void main(String[] args) {
-	while (true) {
-		int number = (int)(Math.random()*)
+	public static void main(String[] args) {
+		int num1, num2;
+		boolean run = true;
+		while (run) {
+			num1 = (int) (Math.random() * 6) + 1;
+			num2 = (int) (Math.random() * 6) + 1;
+			System.out.println("(" + num1 + ", " + num2 + ")");
+			if (num1 + num2 == 5) {
+				run = false;
+			}
+		}
+		System.out.println("프로그램 종료");
 	}
-}
 }
